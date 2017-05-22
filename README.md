4b8475322a7f832d243471c10b4dd16be59ccf50
KunWang <wangkun_ne@163.com>
2017-05-22 19:24:08 +0800

    相关特征提取

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # 数据
 ## 每个文件的样例数据
-
 ```code
 广告 ==> ad.csv <==
 creativeID,adID,camgaignID,advertiserID,appID,appPlatform
+    creativeID (6582L,)
+    adID (3616L,)
+    camgaignID (720L,)
+    advertiserID (91L,)
+    appID (50L,)
+    appPlatform (2L,)
 4079,2318,147,80,14,2
 
 APP类别==> app_categories.csv <==
@@ -32,9 +37,22 @@ instanceID,label,clickTime,creativeID,userID,positionID,connectionType,telecomsO
 
 训练数据==> train.csv <==
 label,clickTime,conversionTime,creativeID,userID,positionID,connectionType,telecomsOperator
+    label (2L,)
+    clickTime (20160L,)
+    conversionTime (18207L,)
+    creativeID (6315L,)
+    userID (2595627L,)
+    positionID (7219L,)
+    connectionType (5L,)
+    telecomsOperator (4L,)
 0,170000,,3089,2798058,293,1,1
 
 提交==> submission.csv <==
 instanceID,prob
 1,0.353741138898
 ```
+
+# 提交记录
+- 2015/5/10 随机: 0.116798
+- 2015/5/22 全0：0.862443
+- 2015/5/22 全1：33.6771
--- feature.py
+++ feature.py
@@ -31,8 +31,7 @@ def get_feature(for_train=True):
     to_drop = []
 
     # creative realated
-    df_ad = read_as_pandas(FILE_AD)
-    df_ad = df_ad[['creativeID', 'appID', 'appPlatform']]
+    df_ad = read_as_pandas(FILE_AD)  # creativeID,adID,camgaignID,advertiserID,appID,appPlatform
     df_app_category = read_as_pandas(FILE_APP_CATEGORIES)
     df_result = pd.merge(df_file, df_ad, how='left', on='creativeID')
     df_result = pd.merge(df_result, df_app_category, how='left', on='appID')
@@ -40,14 +39,10 @@ def get_feature(for_train=True):
     # user realated
     df_user = read_as_pandas(FILE_USER)
     df_user['age'] = np.round(df_user['age'] / 10).astype(int)  # 年龄段
-    df_user['hometown_p'] = np.round(df_user['hometown'].astype(int) / 100).astype(
-        int)  # 取省份
-    df_user['hometown_c'] = np.round(df_user['hometown'].astype(int) % 100).astype(
-        int)  # 城市
-    df_user['residence_p'] = np.round(df_user['residence'].astype(int) / 100).astype(
-        int)  # 取省份
-    df_user['residence_c'] = np.round(df_user['residence'].astype(int) % 100).astype(
-        int)  # 城市
+    df_user['hometown_p'] = np.round(df_user['hometown'].astype(int) / 100).astype(int)  # 取省份
+    df_user['hometown_c'] = np.round(df_user['hometown'].astype(int) % 100).astype(int)  # 城市
+    df_user['residence_p'] = np.round(df_user['residence'].astype(int) / 100).astype(int)  # 取省份
+    df_user['residence_c'] = np.round(df_user['residence'].astype(int) % 100).astype(int)  # 城市
     df_result = pd.merge(df_result, df_user, how='left', on='userID')
 
     to_drop += [
@@ -58,39 +53,73 @@ def get_feature(for_train=True):
     # position related
     df_position = read_as_pandas(FILE_POSITION)
     df_result = pd.merge(df_result, df_position, how='left', on='positionID')
-    # installed app related
-    # 已安装列表是否存在该应用、同类应用的数量
+
+    ## installed app related
+    # 用户已安装列表是否存在该应用、同类应用的数量、所占比例、该用户已经安装app的数量
+    df_installed = read_as_pandas(FILE_USER_INSTALLEDAPPS)
+    df_group_cnt = df_installed.groupby('userID').count().rename(
+        columns={'appID': 'cnt_installed'}).reset_index()  # 每个用户安装的软件总量
+    df_installed_cate = pd.merge(df_installed, df_app_category, how='left', on='appID')
+    df_group_cnt_user_appcate = df_installed_cate.groupby(['userID', 'appCategory']).count().reset_index().rename(
+        columns={'appID': 'cnt_appcate'})
+    df_percent = pd.merge(df_group_cnt_user_appcate, df_group_cnt, how='left',
+                          on='userID')  # userID, appCategory, cnt_installed, cnt_appcate
+    df_percent['cate_percent'] = df_percent['cnt_appcate'].astype(float) / df_percent['cnt_installed']  # cate_percent
+    df_result = pd.merge(df_result, df_percent, how='left',
+                         on=['userID', 'appCategory'])  # + 同类应用的数量、所占比例、该用户已经安装app的数量
+    df_result = df_result.fillna(0)  ## 没有安装过同类应用，则设置为0
+    df_group_exist = df_installed.groupby(['userID', 'appID']).count().rename(
+        columns={'count': 'is_installed'}).reset_index()
+    df_group_exist['is_installed'] = 1
+    df_result = pd.merge(df_result, df_group_exist, how='left', on=['userID', 'appID'])  # + 否存在该应用
+    df_result['is_installed'].fillna(0, inplace=True)  # 1表示已經安裝，0表示沒有安裝
+
+    df_group_app = df_installed.groupby('appID').count().rename(
+        columns={'userID': 'app_installed'}).reset_index()  # app被安装的次数
+    df_result = pd.merge(df_result, df_group_app, on='appID', how='left')
+    df_result['app_installed'].fillna(0, inplace=True)
+
+    # 安裝流水中是否存在该应用
+    df_actions = read_as_pandas(FILE_USER_APP_ACTIONS)
+    df_result['index'] = df_result.index
+    df_merged = pd.merge(df_result, df_actions, on=['userID', 'appID'], how='left')
+    df_merged['action_before'] = df_merged['clickTime'] > df_merged['installTime']
+    df_merged['action_before'] = df_merged['action_before'].astype(int)
+    df_sum = pd.DataFrame(df_merged.loc[:, ['index', 'action_before']]).groupby(['index']).sum().reset_index()
+    df_result['action_installed'] = df_sum['action_before']  ## feautre 安装流水中clickTime之前安装该app的次数
+    df_result.drop(['index'], axis=1, inplace=True)
+    # 修正已安装数据
 
     # 最近是否安装了该应用或者同类应用
-
-    # 该用户已经安装app的数量
 
     # 该app被安装的数量
 
     # context
-    df_result['clickTime_day'] = df_result['clickTime'].astype(str).str.slice(
-        0, 2)
-    df_result['clickTime_hour'] = df_result['clickTime'].astype(str).str.slice(
-        2, 4)
-    df_result['clickTime_minute'] = df_result['clickTime'].astype(
-        str).str.slice(4, 6)
+    df_result['clickTime_day'] = df_result['clickTime'].astype(str).str.slice(0, 2)
+    df_result['clickTime_hour'] = df_result['clickTime'].astype(str).str.slice(2, 4)
+    df_result['clickTime_minute'] = df_result['clickTime'].astype(str).str.slice(4, 6)
+
+    df_result['clickTime_day'] = df_result['clickTime_day'].astype(int)
+    df_result['clickTime_hour'] = df_result['clickTime_hour'].astype(int)
+    df_result['clickTime_minute'] = df_result['clickTime_minute'].astype(int)
 
     # remove unrelated
-    to_drop += ['clickTime', 'creativeID', 'userID', 'positionID', 'appID']
+    to_drop += ['clickTime']  # appID也作为特征，应为很少
 
     if for_train:
         to_drop += ['conversionTime']
 
     df_result.drop(to_drop, axis=1, inplace=True)
+    print df_result.columns
+    print df_result.head(5)
     return df_result, not_ohe
 
 
-def get_tf_feature(gen_ffm=False, with_ohe=True, save=True):
+def get_tf_feature(with_ohe=True, save=True):
     df_train, not_ohe = get_feature(True)
     shuffle(df_train)
     train_y = np.round(df_train['label']).astype(int).values
     df_train.drop('label', 1, inplace=True)
-    print df_train.columns
     df_train.fillna(0, inplace=True)
     train_x = df_train.values
     columns = df_train.columns
@@ -101,87 +130,24 @@ def get_tf_feature(gen_ffm=False, with_ohe=True, save=True):
     df_test.drop(['label', 'instanceID'], axis=1, inplace=True)
     test_x = df_test.values
 
-    if gen_ffm:
-        df_concate = pd.concat([df_train, df_test])
-        list_count = [(c, df_concate[c].unique().shape[0])
-                      for c in df_concate.columns]
-        dict_column2field = {
-            u'connectionType': 0,
-            u'telecomsOperator': 0,
-            u'clickTime_day': 0,
-            u'clickTime_minute': 0,
-            u'clickTime_hour': 0,
-            u'appPlatform': 1,
-            u'appCategory': 1,
-            u'age': 2,
-            u'gender': 2,
-            u'education': 2,
-            u'marriageStatus': 2,
-            u'haveBaby': 2,
-            u'hometown_p': 2,
-            u'hometown_c': 2,
-            u'residence_p': 2,
-            u'residence_c': 2,
-            u'sitesetID': 3,
-            u'positionType': 3,
-        }
-
     if with_ohe:
         idx_to_ohe = [i for i, j in enumerate(columns) if j not in not_ohe]
         encoder = OneHotEncoder(categorical_features=idx_to_ohe)
-        if df_concate is not None:
-            df_concate.fillna(0, inplace=True)
-            np_concate = df_concate.values
-        else:
-            np_concate = np.concatenate([train_x, test_x], axis=0)
-
-        print np_concate.shape
-        encoder.fit(np_concate)
+        df_concate = pd.concat([df_train, df_test], axis=1)
+        df_concate.fillna(-1)
+        encoder.fit(df_concate.values)
 
         train_x = encoder.transform(train_x)
         test_x = encoder.transform(test_x)
 
     print train_x.shape, type(train_x), train_y.shape, type(train_y)
+
     if save:
         pickle.dump(train_x, open('train_x.pkl', 'wb'), 2)
         pickle.dump(train_y, open('train_y.pkl', 'wb'), 2)
 
         pickle.dump(test_x, open('test_x.pkl', 'wb'), 2)
         pickle.dump(inst_id, open('inst_id.pkl', 'wb'), 2)
-
-    if gen_ffm:
-        columns_labels = []
-        for label, count in list_count:
-            columns_labels += [label] * count
-
-        columns_labels = np.asarray(columns_labels)
-
-        def to_fm(filename, data, pre=False):
-            with open(filename, 'w') as f:
-                for i in tqdm(range(data.shape[0])):
-                    row_indice = data.getrow(i).nonzero()
-                    row_values = data[row_indice]
-                    row_field = np.asarray([
-                        dict_column2field[c]
-                        for c in columns_labels[row_indice[1]]
-                    ])
-                    # print row_indice[1].shape, row_values.shape, row_field.shape
-                    line = [
-                        '{}:{}:{}'.format(row_field[i], row_indice[1][i],
-                                          1)
-                        for i in range(len(row_field))
-                    ]
-                    if not pre:
-                        line = [str(train_y[i])] + line
-                    else:
-                        line = [str(-1)] + line
-                    f.write(' '.join(line) + "\n")
-
-                f.write('\n')
-
-        print train_x.shape, test_x.shape
-        to_fm('train_x.ffm', train_x)
-        to_fm('test_x.ffm', test_x, True)
 
     return train_x, train_y, test_x, inst_id
 
@@ -205,4 +171,4 @@ def load_feature(from_file=True, with_ohe=True):
 if __name__ == '__main__':
     # df = get_feature(False)
     # print df.head(5)
-    get_tf_feature(gen_ffm=True)
+    get_tf_feature(with_ohe=False)
