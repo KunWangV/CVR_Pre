ee0b91f516856514ee65b3fba2e295bc45ac2a34
KunWang <wangkun_ne@163.com>
2017-06-22 22:44:25 +0800

    ”fix

--- train.sh
+++ train.sh
@@ -9,22 +9,22 @@ for i in {1..11}
 do
 echo "-------------------------"
 printf -v iter '%02d' $i
-python models/ffm.py fmt_file --cinfo_path column_summary.pkl --file_path cv/train_01.csv --ffm_path cv/ffm_train_01.ffm /
+python models/ffm.py fmt_file --cinfo_path column_summary.pkl --file_path cv/train_${i}.csv --ffm_path cv/ffm_train_${i}.ffm /
 --for_train True
-python models/ffm.py fmt_file --cinfo_path column_summary.pkl --file_path cv/val_01.csv --ffm_path cv/ffm_val_01.ffm /
+python models/ffm.py fmt_file --cinfo_path column_summary.pkl --file_path cv/val_${i}.csv --ffm_path cv/ffm_val_${i}.ffm /
 --for_train True
 
-../libffm/ffm-train -l 0.0001 -k 15 -t 300 -r 0.05 -s 4 --auto-stop -p cv/ffm_val_01.ffm  cv/ffm_train_01.ffm cv/model_01.ffm
+../libffm/ffm-train -l 0.0001 -k 15 -t 300 -r 0.05 -s 4 --auto-stop -p cv/ffm_val_${i}.ffm  cv/ffm_train_${i}.ffm cv/model_${i}.ffm
 
-../libffm/ffm-predict cv/ffm_val_01.ffm cv/model_01.ffm cv/predict_val_01.ffm
-../libffm/ffm-predict test_ffm.ffm cv/model_01.ffm cv/predict_test_01.ffm
+../libffm/ffm-predict cv/ffm_val_${i}.ffm cv/model_${i}.ffm cv/predict_val_${i}.ffm
+../libffm/ffm-predict test_ffm.ffm cv/model_${i}.ffm cv/predict_test_${i}.ffm
 
-tee -i '1i\pred_01' cv/predict_val_01.ffm
-paste -d"," val_label_01.csv cv/predict_val_01.ffm > cv/cmb-predict-val-01.ffm
-tee -i '1d' cv/cmb-predict-val-01.ffm
-cat cv/cmb-predict-val-01.ffm >> cv/pred-val-all.ffm.csv
+tee -i '1i\pred_ffm' cv/predict_val_${i}.ffm
+paste -d"," val_label_${i}.csv cv/predict_val_${i}.ffm > cv/cmb-predict-val-${i}.ffm
+tee -i '1d' cv/cmb-predict-val-${i}.ffm
+cat cv/cmb-predict-val-${i}.ffm >> cv/pred-val-all.ffm.csv
 
-tee -i '1i\pred_01' cv/predict_test_01.ffm
+tee -i '1i\pred_${i}' cv/predict_test_${i}.ffm
 done
 
 
